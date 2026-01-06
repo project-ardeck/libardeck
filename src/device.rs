7ecbcb0007078ab3b8345980a46cb06cef3230a0
akurakuu <akurakuu0725@gmail.com>
2026-01-06 23:46:26 +0900

    ライセンス変更 GPL3.0 -> MIT

--- src/device.rs
+++ src/device.rs
@@ -1,21 +1,3 @@
-/*
-libardeck - This is an all-in-one library for communicating with ardeck and integrating with computers.
-Copyright (C) 2026 Project Ardeck
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-*/
-
 pub mod switch;
 
 use serialport::{SerialPort, SerialPortType, UsbPortInfo};
@@ -30,26 +12,24 @@ pub struct AvailableDeviceInfo {
     pub device_id: String,
 }
 
-impl AvailableDeviceInfo {
-    /// 接続可能なUSB Port一覧を取得する
-    /// # Example
-    /// ```
-    /// let device = AvailableDeviceInfo::list();
-    /// ```
-    pub fn list() -> Vec<Self> {
-        serialport::available_ports()
-            .unwrap_or(Vec::new())
-            .into_iter()
-            .filter_map(|port| match &port.port_type {
-                SerialPortType::UsbPort(e) => Some(Self {
-                    port_name: port.port_name.clone(),
-                    usb_port_info: e.clone(),
-                    device_id: "TODO".into(), // TODO: ID生成
-                }),
-                _ => None,
-            })
-            .collect()
-    }
+/// 接続可能なUSB Port一覧を取得する
+/// # Example
+/// ```
+/// let device = ardeck::device::available_list();
+/// ```
+pub fn available_list() -> Vec<AvailableDeviceInfo> {
+    serialport::available_ports()
+        .unwrap_or(Vec::new())
+        .into_iter()
+        .filter_map(|port| match &port.port_type {
+            SerialPortType::UsbPort(e) => Some(AvailableDeviceInfo {
+                port_name: port.port_name.clone(),
+                usb_port_info: e.clone(),
+                device_id: "TODO".into(), // TODO: ID生成
+            }),
+            _ => None,
+        })
+        .collect()
 }
 
 /// デバイス一覧の実装
@@ -61,7 +41,7 @@ impl AvailableDeviceInfoList for Vec<AvailableDeviceInfo> {
     /// デバイス一覧のうち、arduinoのベンダーコードを持つデバイスだけを抽出する
     /// # Example
     /// ```
-    /// let device = AvailableDeviceInfo::list().arduino_only();
+    /// let device = ardeck::device::available_list().arduino_only();
     /// ```
     fn arduino_only(self) -> Vec<AvailableDeviceInfo> {
         self.into_iter()
@@ -118,7 +98,7 @@ impl ArdeckConnection {
         handler: Option<ArdeckConnectionHandler>,
     ) -> Self {
         match serialport::new(port_name, baud_rate).open() {
-            Ok(p) => ArdeckConnection {
+            Ok(p) => Self {
                 device_id,
                 handler,
                 serialport: Some(p),
